b5a7b0b8e81eb8ab49851afc676897bd42586011
str1k <y.thanasit@gmail.com>
2017-10-30 22:10:01 +0700

    add comment

--- checker.bat
+++ checker.bat
@@ -23,9 +23,10 @@ FOR /L %%A IN (1,1,10) DO (
 )
 
 :check_pool
-rem Put All your pool address here
 echo checking pool connection
 setlocal
+rem Check conectivity for every address in the servers list, delimit each address by space.
+rem use PortQryV2 to check connectivity, if any of the address work proceed to check GPUs number.
 for %%I in (%servers%) do (
     for /f "tokens=1,2 delims=:" %%a in ("%%I") do (
         set "port=%%~b"
@@ -46,6 +47,8 @@ goto :restart_PC
 
 :check_gpu
 echo %%a port !port!: OK
+rem Get all GPU list but skip On-board card and concat all name to GPU_NAME, 
+rem also count all the number of GPU.
 SET /A CARDS=0
 for /F "tokens=* skip=1" %%n in ('WMIC path Win32_VideoController get Name ^| findstr "."') do (
 	set GPU_NAME=%%n
@@ -54,7 +57,7 @@ for /F "tokens=* skip=1" %%n in ('WMIC path Win32_VideoController get Name ^| fi
 echo %GPU_NAME%
 SET /A CARDS = CARDS - ONBOARD_GPU
 echo Detected %CARDS% Cards
-rem Check if all card existed
+rem Check if all card existed, if it does proceed to run miner, else restart PC.
 if %CARDS% == %GPU_NUM% (
 	goto :run_minner
 ) else (
